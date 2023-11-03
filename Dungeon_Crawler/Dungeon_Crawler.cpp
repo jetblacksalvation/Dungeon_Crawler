13343b13c4be47276c4005433458dba2b724047a
coolanimeguy666 <coolanimeguy666@gmail.com>
2023-11-03 02:42:27 -0700

    added some good code

--- Dungeon_Crawler/Dungeon_Crawler.cpp
+++ Dungeon_Crawler/Dungeon_Crawler.cpp
@@ -1,73 +1,17 @@
-#include <iostream>
-#include <Windows.h>
-#include <chrono>
-HANDLE ConsoleOutputHandle = GetStdHandle(((DWORD)-11));
-HANDLE ConsoleInputHandle = GetStdHandle(STD_INPUT_HANDLE);
-class GameWorld {
-public:
-    GameWorld() {
-        worldMessage = L"Loading World";
-
-    }
-    void outWorldMessage(){
-        using namespace std::chrono;
-
-        auto startTime = high_resolution_clock::now();
-
-        while (true) {
-            // Get the current time point
-            auto currentTime = high_resolution_clock::now();
-
-            // Calculate the elapsed time
-            auto elapsedTime = duration_cast<duration<double>>( currentTime- startTime ).count();
-
-            //std::cout << elapsedTime << ": is time\n";
-            // Check if the desired time has passed
-            if (elapsedTime >= messageTime) {
-                break;
-            }
-            outputWideString(this->worldMessage, 0, 0);
-        }
-
-    }
-    //static because this is need not be bound to an instance of the class.
-    static void outputWideString(std::wstring c, int x, int y) {
-        LPDWORD a = (LPDWORD) new LPDWORD;
-
-        for (SHORT index = 0; index < c.size(); index++) {
-            _COORD cordinates = { x,y };
-
-            cordinates.X += index;
-            FillConsoleOutputCharacterA(ConsoleOutputHandle, c[index], 1, cordinates, a);
-        }
-        delete a;
-    };
-private:
-    std::wstring worldMessage = L"";
-    clock_t bufferTime        = clock();
-    double messageTime        = 1;
-};
+﻿#include "Game.h"
 
 int main()
 {
     //std::wstring c = L"abcdef";
-    //INPUT_RECORD irec;
+    //INPUT_RECORD irec; → ← ↑ ↓ ↕ ↨ ↔  
     //LPDWORD a = (LPDWORD) new LPDWORD;
     auto game = GameWorld();
 
-    game.outWorldMessage();
+    game.mainLoop();
     //while (true) {
     //    outputWideString(c);
 
-    //    ReadConsoleInput(ConsoleInputHandle, &irec, 1, a);
-    //    if (irec.EventType == KEY_EVENT
-    //        && ((KEY_EVENT_RECORD&)irec.Event).bKeyDown
-    //        )//&& ! ((KEY_EVENT_RECORD&)irec.Event).wRepeatCount )
-    //    {
-    //        auto k = (KEY_EVENT_RECORD&)irec.Event;
-    //        //std::cout << k.uChar.AsciiChar << "\n";
-    //        c[0] = k.uChar.UnicodeChar;
-    //    }
+
 
     //}
 
---
+++ Dungeon_Crawler/Game.cpp
@@ -0,0 +1,91 @@
+﻿#include "Game.h"
+GameWorld::GameWorld() {
+    worldMessage = L"Loading World";
+    //removes the cursor 
+    CONSOLE_CURSOR_INFO     cursorInfo;
+    GetConsoleCursorInfo(ConsoleOutputHandle, &cursorInfo);
+    cursorInfo.bVisible = false; // set the cursor visibility
+    SetConsoleCursorInfo(ConsoleOutputHandle, &cursorInfo);
+};
+void GameWorld::mainLoop() {
+    int x = 0;
+    INPUT_RECORD irec;
+    LPDWORD a = (LPDWORD) new LPDWORD;
+    while (true) {
+        ReadConsoleInput(ConsoleInputHandle, &irec, 1, a);
+
+        outWorldMessage();
+        drawPlayer();
+        //x++;
+        if (irec.EventType == KEY_EVENT
+            && ((KEY_EVENT_RECORD&)irec.Event).bKeyDown
+            )//&& ! ((KEY_EVENT_RECORD&)irec.Event).wRepeatCount )
+        {
+            auto k = (KEY_EVENT_RECORD&)irec.Event;
+            //std::cout << k.uChar.AsciiChar << "\n";
+            if (k.uChar.AsciiChar == 'w') {
+
+            }
+            this->player.handleInput(k.uChar.AsciiChar);
+        }
+
+        //this->player.setDirection(Player::DIRECTIONS(x%4));
+    }
+}
+void GameWorld::drawPlayer() {
+    
+    auto direction = this->player.getDirection();
+    GameWorld::outputWideString(direction, player.PlayerCoordinates);
+}
+void GameWorld::outWorldMessage() {
+    using namespace std::chrono;
+
+    auto startTime = high_resolution_clock::now();
+    SetCursor(NULL);
+    while (true) {
+        // Get the current time point
+        auto currentTime = high_resolution_clock::now();
+
+        // Calculate the elapsed time
+        auto elapsedTime = duration_cast<duration<double>>(currentTime - startTime).count();
+
+        //std::cout << elapsedTime << ": is time\n";
+        // Check if the desired time has passed
+        if (elapsedTime >= messageTime) {
+            break;
+        }
+        outputWideString(this->worldMessage, 0, 0);
+    }
+
+};
+//static functions------
+int GameWorld::outputWideString(std::wstring c, int x, int y) {
+    //returns number of characters drawn.
+    LPDWORD a = (LPDWORD) new LPDWORD;
+
+    for (SHORT index = 0; index < c.size(); index++) {
+        _COORD cordinates = { x,y };
+
+        cordinates.X += index;
+        FillConsoleOutputCharacterW(ConsoleOutputHandle, c[index], 1, cordinates, a);
+    }
+    int ret = (int)a;
+
+    delete a;
+    return ret;
+};
+int GameWorld::outputWideString(std::wstring c, COORD pos) {
+    //returns number of characters drawn.
+    LPDWORD a = (LPDWORD) new LPDWORD;
+
+    for (SHORT index = 0; index < c.size(); index++) {
+        _COORD cordinates = pos;
+
+        cordinates.X += index;
+        FillConsoleOutputCharacterW(ConsoleOutputHandle, c[index], 1, cordinates, a);
+    }
+    int ret = (int)a;
+
+    delete a;
+    return ret;
+};
